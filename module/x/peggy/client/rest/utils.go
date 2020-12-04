b4c72a0d3d3c37f49df67b015623a512e6391386
Segue <huoda.china@163.com>
2020-12-04 16:07:09 +0800

    fix typo

--- module/x/peggy/client/rest/utils.go
+++ module/x/peggy/client/rest/utils.go
@@ -10,13 +10,13 @@ type PeggyBootstrapProposalReq struct {
 
 	Title                string         `json:"title" yaml:"title"`
 	Description          string         `json:"description" yaml:"description"`
-	PeggyId              string         `json:"peggy_id" yaml:"peggy_id"`
+	PeggyID              string         `json:"peggy_id" yaml:"peggy_id"`
 	ProxyContractHash    string         `json:"proxy_contract_hash" yaml:"proxy_contract_hash"`
 	ProxyContractAddress string         `json:"proxy_contract_address" yaml:"proxy_contract_address"`
 	LogicContractHash    string         `json:"logic_contract_hash" yaml:"logic_contract_hash"`
 	LogicContractAddress string         `json:"logic_contract_address" yaml:"logic_contract_address"`
 	StartThreshold       uint64         `json:"start_threshold" yaml:"start_threshold"`
-	BridgeChainId        uint64         `json:"bridge_chain_id" yaml:"bridge_chain_id"`
+	BridgeChainID        uint64         `json:"bridge_chain_id" yaml:"bridge_chain_id"`
 	BootstrapValsetNonce uint64         `json:"bootstrap_valset_nonce" yaml:"bootstrap_valset_nonce"`
 	Proposer             sdk.AccAddress `json:"proposer" yaml:"proposer"`
 	Deposit              sdk.Coins      `json:"deposit" yaml:"deposit"`
